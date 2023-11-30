2bd67769e4fbf39b9d55d46ae105780fd3244dc5
Rystambek <rustambekjumaqulov@gmail.com>
2023-11-30 23:45:37 +0500

    ulrs

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
-from app.views.car import CarView
+from app.views.car import CarView,CarIDView,all,CarallView
+from app.views.company import CompanyView
 
 
 urlpatterns = [
-    path('car/<int:id>',CarView.as_view()),
-    path('car/',CarView.as_view()),
+    path('company/<int:id>/car/',CarView.as_view()),
+    path('car/',CarallView.as_view()),
+    path('company/<int:id>',CompanyView.as_view()),
+    path("company/",CompanyView.as_view()),
+    path("company/<int:car_id>/car/<int:id>",CarIDView.as_view()),
     path('',all)
 ]
